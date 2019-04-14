8971a3894e21e57ba625bb49fcbb6f6ae6b4a158
Emilio Coppa <ercoppa@gmail.com>
2019-04-14 22:59:47 +0200

    glob

--- list_files.rs
+++ list_files.rs
@@ -6,14 +6,15 @@ use glob::glob;
 
 fn main() {
     for entry in glob("./*.rs").expect("Failed to read glob pattern") {
-        let f = entry.expect("Error");
-        let f = f.to_str().expect("Error to_str()");
-        println!("File: {}", f);
         /*
         match entry {
             Ok(path) => println!("{:?}", path.display()),
             Err(e) => println!("{:?}", e),
         }
         */
+
+        let f = entry.expect("Error");
+        let f = f.to_str().expect("Error to_str()");
+        println!("File: {}", f);
     }
 }
